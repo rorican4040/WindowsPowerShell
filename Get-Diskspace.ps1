7baba26d7698a857e2f0749893d1c0efde14928f
rorican4040 <rorican4040@yahoo.com>
2017-05-17 07:12:58 -0400

    Add files via upload

---
+++ Get-Diskspace.ps1
@@ -0,0 +1,2 @@
+﻿$RemoteMachineName = Read-Host 'Enter the remote machine name'
+Get-CimInstance -computername $RemoteMachineName Win32_LogicalDisk -Filter "DeviceID='C:'" | select @{n='freegb';e={$_.FreeSpace / 1GB -as [int]}}
